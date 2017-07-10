e89c4d0648c370e88f51df0e8f240861178f2da0
Jesse Tyner-Bryan <jesse@Jesses-MacBook-Air.local>
2017-07-10 17:08:17 -0400

    updating hangman

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Game
 
-WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
+WORDS = ["abstract", "water", "turing", "glasses", "ribald", "bagpipes", "fervid", "gazebo", "klutz", "ostracize", "phlegm", "yacht", "squawk", "quip", "matrix", "astoria", "brooklyn"]
 
 	def initialize
 		@turns = 7
@@ -10,6 +10,15 @@ WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
 		@user = User.new
 		@wins = @user.wins
 		@losses = @user.losses
+		@hangman_array = [
+		"+---+-",
+	 	"|   |",
+	 	"|   0",
+	 	"|  /|\\",
+	 	"|   /\\",
+	 	"|      ",
+	 	"+---+-"
+		]
 	end
 
 	def greeting
@@ -17,7 +26,7 @@ WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
 		@user.name = gets.chomp.capitalize
 		puts "Hi, #{@user.name}! The objective of this game is to guess the hidden word before you run out of turns and die."
 	end
-		
+
 	def play?
 		puts "You currentlty have #{@wins} wins and #{@losses} losses. Would you like to play? y/n"
 		user_response = gets.chomp
@@ -37,19 +46,48 @@ WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
 	def display_word
 		@word = WORDS.sample.split(//)
 		puts "Your word is #{@word.length} letters long."
+		@word.size.times do
+			print "__ "
+		end
+	end
+
+	def display_hangman
+		(0..(6 - @turns)).to_a.each{|line_number| puts @hangman_array[line_number]}
 	end
 
 	def pick_a_letter
-		puts "Pick a letter from A to Z, or guess the entire word."
-		@guess = gets.chomp
+		puts "\nPick a letter from A to Z, or guess the whole word."
+		@guess = gets.downcase.chomp
+		if @correct_guess.include?(@guess)
+			puts "You've already correctly guessed that letter, please try something else"
+			pick_a_letter
+		elsif (@guess =~ /[[:alpha:]]/) != 0
+			puts "Letters only, please"
+			pick_a_letter
+		end
+	end
+
+	def print_word
+		@word.each_with_index do |l, i|
+			if @correct_guess.include?(l)
+				print "#{l}  "
+			else
+				print "  "
+			end
+		end
 	end
 
 	def guess_correctly?
-		@word.include?(@guess)
+		@word.include?(@guess) || @word == @guess.split(//)
 	end
 
 	def correct_guess?
-		if guess_correctly?
+		if @guess.split(//).sort == @word.sort
+			puts "You win! The word is '#{@word.join}'."
+			@wins +=1
+			@correct_guess.clear
+			play?
+		elsif guess_correctly?
 			indexes = @word.each_index.select { |i| @word[i] == @guess}
 			indexes = indexes.map {|i| i + 1}
 			indexes.size.times do
@@ -59,18 +97,19 @@ WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
 		else
 			@turns -= 1
 			puts "You did not guess correctly. You have #{@turns} turn(s) remaining."
+			display_hangman
 		end
 	end
 
 	def won_or_lost?
-		if @word.sort == @correct_guess.sort
+		if @word.sort.uniq == @correct_guess.sort.uniq
 			puts "You win! The word is '#{@word.join}'."
 			@wins +=1
 			@correct_guess.clear
 			play?
 		elsif
 			@turns == 0
-			puts "You're out of turns. You've been hanged. Would you like to try again with a new life?"
+			puts "\nThe word was '#{@word.join}'. You're out of turns. You've been hanged. Would you like to try again with a new life?"
 			@losses +=1
 			play?
 		end
@@ -82,6 +121,7 @@ WORDS = ["abstract", "water", "turing", "glasses", "ribald"]
 		until won_or_lost? 
 			pick_a_letter
 			correct_guess?
+			print_word
 			won_or_lost?
 		end
 	end
--- bin/run.rb
+++ bin/run.rb
@@ -1,9 +1,7 @@
 #!/usr/bin/env ruby
 
-
 require 'pry'
 require_relative '../config/environment.rb'
-
 
 game = Game.new
 game.full_game
